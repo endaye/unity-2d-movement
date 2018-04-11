29ed9486a23a2d3d1ae2f50f884b2ff1bd422c76
Yuancheng Zhang <old.far.hot@gmail.com>
2018-04-11 09:52:43 -0500

    Tracking button release states

--- Assets/Scripts/InputState.cs
+++ Assets/Scripts/InputState.cs
@@ -5,28 +5,13 @@ using UnityEngine;
 public class ButtonState
 {
     public bool value;
-
+    public float holdTime = 0f;
 }
 
 public class InputState : MonoBehaviour
 {
 
     private Dictionary<Buttons, ButtonState> buttonStates = new Dictionary<Buttons, ButtonState>();
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        foreach (var state in buttonStates)
-        {
-            Debug.Log("Button State " + state.Key + " " + state.Value.value);
-        }
-    }
 
     public void SetButtonValue(Buttons key, bool value)
     {
@@ -36,6 +21,18 @@ public class InputState : MonoBehaviour
         }
 
         var state = buttonStates[key];
+
+        if (state.value && !value)
+        {
+            Debug.Log("Button " + key + " released " + state.holdTime);
+            state.holdTime = 0f;
+        }
+        else if (state.value && value)
+        {
+            state.holdTime += Time.deltaTime;
+            Debug.Log("Button " + key + " down " + state.holdTime);
+        }
+
         state.value = value;
     }
 }
