30ad6165f1877269833ea45e77e159b95d09a5a5
Yuancheng Zhang <old.far.hot@gmail.com>
2018-05-16 03:08:41 -0500

    Making the player jump

---
+++ Assets/Scripts/Behaviors/Jump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : AbstractBehavior
+{
+
+    public float jumpSpeed = 200f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var canJump = inputState.GetButtonValue(InputButtons[0]);
+
+	    if (collisionState.standing)
+	    {
+	        if (canJump)
+	        {
+                OnJump();
+	        }
+	    }
+	}
+
+    protected virtual void OnJump()
+    {
+        var vel = body2d.velocity;
+
+        body2d.velocity = new Vector2(vel.x, jumpSpeed);
+
+    }
+}
