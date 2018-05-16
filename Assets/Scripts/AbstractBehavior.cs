f4a7e0fbd62faf089e28ebe5941feeb645f8ac2b
Yuancheng Zhang <old.far.hot@gmail.com>
2018-05-16 01:13:43 -0500

    Building the abstract player behavior

---
+++ Assets/Scripts/AbstractBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractBehavior : MonoBehaviour
+{
+
+    public Buttons[] InputButtons;
+
+    protected InputState inputState;
+    protected Rigidbody2D body2d;
+
+    protected virtual void Awake()
+    {
+        inputState = GetComponent<InputState>();
+        body2d = GetComponent<Rigidbody2D>();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
