a7ab247988a66c2ae4cf4193a034172c6e0b02d6
Yuancheng Zhang <old.far.hot@gmail.com>
2018-05-16 02:40:08 -0500

    Building a collision state manager

---
+++ Assets/Scripts/Collision/CollisionState.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionState : MonoBehaviour
+{
+
+    public LayerMask collisionLayer;
+
+    public bool standing;
+    public Vector2 bottomPosition = Vector2.zero;
+    public float collisionRadius = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        var pos = bottomPosition;
+        pos.x += transform.position.x;
+        pos.y += transform.position.y;
+
+        standing = Physics2D.OverlapCircle(pos, collisionRadius, collisionLayer);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
