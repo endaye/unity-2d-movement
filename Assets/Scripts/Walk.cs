d2e82a74e77649e9a68fbe03296ded67feb069f5
Yuancheng Zhang <old.far.hot@gmail.com>
2018-05-16 01:52:50 -0500

    Adding walking to the player

---
+++ Assets/Scripts/Walk.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : AbstractBehavior {
+
+	public float speed = 50f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var right = inputState.GetButtonValue (InputButtons [0]);
+		var left = inputState.GetButtonValue (InputButtons [1]);
+
+		if (right || left) {
+			var tmpSpeed = speed;
+			var velX = tmpSpeed * (float)inputState.direction;
+			body2d.velocity = new Vector2 (velX, body2d.velocity.y);
+		}
+	}
+}
