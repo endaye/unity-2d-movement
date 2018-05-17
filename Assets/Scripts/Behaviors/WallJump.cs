cc9b0c8c24bc0ee9203504c7173ac70a9d2602fb
Yuancheng Zhang <old.far.hot@gmail.com>
2018-05-17 17:22:19 -0500

    Making the player jump off walls.

---
+++ Assets/Scripts/Behaviors/WallJump.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallJump : AbstractBehavior
+{
+    public Vector2 jumpVelocity = new Vector2(50f, 200f);
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (collisionState.onWall && !collisionState.standing)
+        {
+            var canJump = inputState.GetButtonValue(InputButtons[0]);
+            if (canJump)
+            {
+                inputState.direction = inputState.direction == Directions.Right ? Directions.Left : Directions.Right;
+                body2d.velocity = new Vector2(jumpVelocity.x * (float)inputState.direction, jumpVelocity.y);
+            }
+        }
+    }
+}
