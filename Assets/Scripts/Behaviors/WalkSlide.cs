c16ab6a97d6b7293cb1d86c7fd94461d00d05b03
Yuancheng Zhang <old.far.hot@gmail.com>
2018-05-17 12:21:38 -0500

    Sliding down walls

---
+++ Assets/Scripts/Behaviors/WalkSlide.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkSlide : StickToWall
+{
+
+    public float slideVelocity = -5f;
+
+    // Update is called once per frame
+    override protected void Update()
+    {
+        base.Update();
+
+        if (onWallDetected)
+        {
+            var velY = slideVelocity;
+            body2d.velocity = new Vector2(body2d.velocity.x, velY);
+        }
+    }
+
+    override protected void OnStick()
+    {
+        body2d.velocity = Vector2.zero;
+    }
+
+    override protected void OffWall()
+    {
+        // do nothing
+    }
+}
