7e333c5379d02d58edd37ad4a64b21b33cac0c6c
Yuancheng Zhang <old.far.hot@gmail.com>
2018-05-17 17:28:57 -0500

    Cleaning up the wall jump

--- Assets/Scripts/Behaviors/WallJump.cs
+++ Assets/Scripts/Behaviors/WallJump.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class WallJump : AbstractBehavior
 {
     public Vector2 jumpVelocity = new Vector2(50f, 200f);
+    public bool jumpingOffWall;
+    public float resetDelay = .2f;
+
+    private float timeElapsed = 0f;
 
     // Update is called once per frame
     void Update()
@@ -12,10 +16,25 @@ public class WallJump : AbstractBehavior
         if (collisionState.onWall && !collisionState.standing)
         {
             var canJump = inputState.GetButtonValue(InputButtons[0]);
-            if (canJump)
+            if (canJump && !jumpingOffWall)
             {
                 inputState.direction = inputState.direction == Directions.Right ? Directions.Left : Directions.Right;
                 body2d.velocity = new Vector2(jumpVelocity.x * (float)inputState.direction, jumpVelocity.y);
+
+                ToggleScripts(false);
+                jumpingOffWall = true;
+            }
+        }
+
+        if (jumpingOffWall)
+        {
+            timeElapsed += Time.deltaTime;
+
+            if (timeElapsed > resetDelay)
+            {
+                ToggleScripts(true);
+                jumpingOffWall = false;
+                timeElapsed = 0f;
             }
         }
     }
