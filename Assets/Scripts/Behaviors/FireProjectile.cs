2a1c9f31f25bb8122e6a5580d1433433c14fc870
Yuancheng Zhang <old.far.hot@gmail.com>
2018-05-17 18:54:35 -0500

    Cleaning up the fireball projectile

--- Assets/Scripts/Behaviors/FireProjectile.cs
+++ Assets/Scripts/Behaviors/FireProjectile.cs
@@ -6,6 +6,9 @@ public class FireProjectile : AbstractBehavior
 {
     public float shootDelay = .5f;
     public GameObject projectilePrefab;
+    public Vector2 firePosition = Vector2.zero;
+    public Color debugColor = Color.yellow;
+    public float debugRadius = 3f;
 
     private float timeElapsed = 0f;
 
@@ -16,7 +19,7 @@ public class FireProjectile : AbstractBehavior
 	        var canFire = inputState.GetButtonValue(InputButtons[0]);
 	        if (canFire && timeElapsed > shootDelay)
 	        {
-                CreateProjectile(transform.position);
+                CreateProjectile(CalculateFirePosition());
 	            timeElapsed = 0f;
 	        }
 
@@ -24,9 +27,32 @@ public class FireProjectile : AbstractBehavior
 	    }
 	}
 
+    Vector2 CalculateFirePosition()
+    {
+        var pos = firePosition;
+        pos.x *= (float) inputState.direction;
+        pos.x += transform.position.x;
+        pos.y += transform.position.y;
+        return pos;
+    }
+
     public void CreateProjectile(Vector2 pos)
     {
         var clone = Instantiate(projectilePrefab, pos, Quaternion.identity) as GameObject;
         clone.transform.localScale = transform.localScale;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = debugColor;
+        var pos = firePosition;
+        if (inputState != null)
+        {
+            pos.x *= (float)inputState.direction;
+        }
+        pos.x += transform.position.x;
+        pos.y += transform.position.y;
+        
+        Gizmos.DrawWireSphere(pos, debugRadius);
     }
 }
