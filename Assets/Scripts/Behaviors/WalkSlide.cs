a0a495f8f70ec7fc154643f7feff97e4b3ea8023
Yuancheng Zhang <old.far.hot@gmail.com>
2018-05-17 16:26:31 -0500

    Adding a wall slide effect

--- Assets/Scripts/Behaviors/WalkSlide.cs
+++ Assets/Scripts/Behaviors/WalkSlide.cs
@@ -7,13 +7,17 @@ public class WalkSlide : StickToWall
 
     public float slideVelocity = -5f;
     public float slideMultiplier = 5f;
+    public GameObject dustPrefab;
+    public float dustSpawnDelay = .5f;
+
+    private float timeElapsed = 0f;
 
     // Update is called once per frame
     override protected void Update()
     {
         base.Update();
 
-        if (onWallDetected)
+        if (onWallDetected && !collisionState.standing)
         {
             var velY = slideVelocity;
             if (inputState.GetButtonValue(InputButtons[0]))
@@ -21,6 +25,18 @@ public class WalkSlide : StickToWall
                 velY *= slideMultiplier;
             }
             body2d.velocity = new Vector2(body2d.velocity.x, velY);
+
+            if (timeElapsed > dustSpawnDelay)
+            {
+                var dust = Instantiate(dustPrefab);
+                var pos = transform.position;
+                pos.y += 2f;
+                dust.transform.position = pos;
+                dust.transform.localScale = transform.localScale;
+                timeElapsed = 0f;
+            }
+
+            timeElapsed += Time.deltaTime;
         }
     }
 
@@ -31,6 +47,6 @@ public class WalkSlide : StickToWall
 
     override protected void OffWall()
     {
-        // do nothing
+        timeElapsed = 0f;
     }
 }
